0a6721e6b487e244df6d58c366a080f49570523d
mukankunziza opportunee <nshutioppo@yahoo.fr>
2019-01-19 23:10:47 +0200

    add link

---
+++ css/js/scripts.js
@@ -0,0 +1,32 @@
+
+
+var student;
+$(document).ready(function(){
+    $("button#new").click(function(){
+    $(".bank").hide();
+    $(".container1").show();
+
+    var name =$('#namee').val();
+    
+    $('#dg').text(name);
+
+    var student =$('#initial').val();
+    console.log(student)
+    $('#mg').text(student);
+
+    $("#submit").click(function(){
+        var kub = parseInt($('#kubitsa').val());
+        var withdr = parseInt($('#kubikuza').val());
+        
+        var sum = kub + parseInt(student);
+        $('#mg').text(sum);
+        // var sub =parseInt(student)-withdr;
+        // $('#mg').text(sub);
+
+       
+    });
+
+});
+
+});
+
